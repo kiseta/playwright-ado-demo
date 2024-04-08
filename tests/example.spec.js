55f321268cc63d1c1a117b7273637f23b7916db6
Tatyana Kiseleva <tatyana.kiseleva@teck.com>
2024-04-08 15:55:13 -0700

    added console log statement for global BASE_URL

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -4,12 +4,16 @@ const { test, expect } = require('@playwright/test');
 test('has title', async ({ page }) => {
   await page.goto(global.BASE_URL);
 
+  console.log('Test 1 [has title] : Global BASE_URL variable: ', global.BASE_URL)
+
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
   await page.goto(global.BASE_URL);
+
+  console.log('Test 2 [get started link]: Global BASE_URL variable: ', global.BASE_URL)
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
